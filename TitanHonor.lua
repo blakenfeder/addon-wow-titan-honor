6a738f3f2516b2d210e727ff29b6c6cb1cde0b3c
Blakenfeder <git@blakenfeder.com>
2022-10-26 01:53:17 -0300

    Update to 100000 and add new features and fixes
     * Set currency count color to red when matching the cap
    
     * Ensure tooltip title color matches currency rarity
    
     * Fix error when currency was not yet discovered

--- TitanHonor.lua
+++ TitanHonor.lua
@@ -17,12 +17,7 @@ local TitanHonor = {
 function TitanHonor.GetCurrencyInfo()
   local i = 0
   for i = 1, C_CurrencyInfo.GetCurrencyListSize(), 1 do
-    info = C_CurrencyInfo.GetCurrencyListInfo(i)
-    
-    -- if (not TitanHonor.IsInitialized and DEFAULT_CHAT_FRAME) then
-    --   print(info.name, tostring(info.iconFileID))
-    -- end
-    
+    info = C_CurrencyInfo.GetCurrencyListInfo(i)    
     if tostring(info.iconFileID) == "1455894" then
       return info
     end
@@ -41,31 +36,43 @@ TitanHonor.Const.Version = GetAddOnMetadata(TitanHonor.Const.Name, "Version")
 TitanHonor.Const.Author = GetAddOnMetadata(TitanHonor.Const.Name, "Author")
 
 -- Text colors (AARRGGBB)
-local BKFD_C_BLUE_ITEM = "|cff0070dd"
 local BKFD_C_BURGUNDY = "|cff993300"
 local BKFD_C_GRAY = "|cff999999"
 local BKFD_C_GREEN = "|cff00ff00"
 local BKFD_C_ORANGE = "|cffff8000"
+local BKFD_C_RED = "|cffff0000"
 local BKFD_C_WHITE = "|cffffffff"
 local BKFD_C_YELLOW = "|cffffcc00"
+
+-- Text item colors (AARRGGBB)
+local BKFD_C_COMMON = "|cffffffff"
+local BKFD_C_UNCOMMON = "|cff1eff00"
+local BKFD_C_RARE = "|cff0070dd"
+local BKFD_C_EPIC = "|cffa335ee"
+local BKFD_C_LEGENDARY = "|cffff8000"
+local BKFD_C_ARTIFACT = "|cffe5cc80"
+local BKFD_C_BLIZZARD = "|cff00ccff"
 
 -- Load Library references
 local LT = LibStub("AceLocale-3.0"):GetLocale("Titan", true)
 local L = LibStub("AceLocale-3.0"):GetLocale(TitanHonor.Const.Id, true)
 
 -- Currency update variables
-local BKFD_RA_UPDATE_FREQUENCY = 0.0
+local BKFD_HO_UPDATE_FREQUENCY = 0.0
 local currencyCount = 0.0
 local currencyMaximum
+local seasonalCount = 0.0
+local isSeasonal = false
+local currencyDiscovered = false
 
 function TitanPanelHonorButton_OnLoad(self)
   self.registry = {
     id = TitanHonor.Const.Id,
     category = "Information",
     version = TitanHonor.Const.Version,
-    menuText = L["BKFD_TITAN_RA_MENU_TEXT"], 
+    menuText = L["BKFD_TITAN_HO_MENU_TEXT"], 
     buttonTextFunction = "TitanPanelHonorButton_GetButtonText",
-    tooltipTitle = BKFD_C_BLUE_ITEM..L["BKFD_TITAN_RA_TOOLTIP_TITLE"],
+    tooltipTitle = BKFD_C_RARE..L["BKFD_TITAN_HO_TOOLTIP_TITLE"],
     tooltipTextFunction = "TitanPanelHonorButton_GetTooltipText",
     icon = "Interface\\Icons\\achievement_legionpvptier4",
     iconWidth = 16,
@@ -89,46 +96,73 @@ end
 function TitanPanelHonorButton_GetButtonText(id)
   local currencyCountText
   if not currencyCount then
-    currencyCountText = "??"
+    currencyCountText = "0"
   else  
     currencyCountText = TitanHonor.Util_GetFormattedNumber(currencyCount)
   end
 
-  return L["BKFD_TITAN_RA_BUTTON_LABEL"], TitanUtils_GetHighlightText(currencyCountText)
+  if (currencyMaximum and not(currencyMaximum == 0) and currencyCount and currencyMaximum == currencyCount) then
+    currencyCountText = BKFD_C_RED..currencyCountText
+  end
+
+  return L["BKFD_TITAN_HO_BUTTON_LABEL"], TitanUtils_GetHighlightText(currencyCountText)
 end
 
 function TitanPanelHonorButton_GetTooltipText()
+  if (not currencyDiscovered) then
+    return
+      L["BKFD_TITAN_HO_TOOLTIP_DESCRIPTION"].."\r"..
+      " \r"..
+      TitanUtils_GetHighlightText(L["BKFD_TITAN_HO_TOOLTIP_NOT_YET_DISCOVERED"])
+  end
 
-  local totalLabel = L["BKFD_TITAN_RA_TOOLTIP_COUNT_LABEL"]
+  -- Set which total value will be displayed
+  local tooltipCurrencyCount = currencyCount
+  if (isSeasonal) then
+    tooltipCurrencyCount = seasonalCount
+  end
+
+  -- Set how the total value will be displayed
   local totalValue = string.format(
     "%s/%s",
-    TitanHonor.Util_GetFormattedNumber(currencyCount),
+    TitanHonor.Util_GetFormattedNumber(tooltipCurrencyCount),
     TitanHonor.Util_GetFormattedNumber(currencyMaximum)
   )
-  if(not currencyMaximum or currencyMaximum == 0) then
-    totalLabel = L["BKFD_TITAN_RA_TOOLTIP_COUNT_LABEL_TOTAL"]
+  if (not currencyMaximum or currencyMaximum == 0) then
     totalValue = string.format(
       "%s",
-      TitanHonor.Util_GetFormattedNumber(currencyCount)
+      TitanHonor.Util_GetFormattedNumber(tooltipCurrencyCount)
     )
+  elseif (currencyMaximum == tooltipCurrencyCount) then
+    totalValue = BKFD_C_RED..totalValue
+  end
+  
+  local totalLabel = L["BKFD_TITAN_HO_TOOLTIP_COUNT_LABEL_TOTAL_MAXIMUM"]
+  if (isSeasonal) then
+    totalLabel = L["BKFD_TITAN_HO_TOOLTIP_COUNT_LABEL_TOTAL_SEASONAL"]
+  elseif (not currencyMaximum or currencyMaximum == 0) then
+    totalLabel = L["BKFD_TITAN_HO_TOOLTIP_COUNT_LABEL_TOTAL"]
   end
 
   return
-    L["BKFD_TITAN_RA_TOOLTIP_DESCRIPTION"].."\r"..
+    L["BKFD_TITAN_HO_TOOLTIP_DESCRIPTION"].."\r"..
     " \r"..
     totalLabel..TitanUtils_GetHighlightText(totalValue)
 end
 
 function TitanPanelHonorButton_OnUpdate(self, elapsed)
-  BKFD_RA_UPDATE_FREQUENCY = BKFD_RA_UPDATE_FREQUENCY - elapsed;
+  BKFD_HO_UPDATE_FREQUENCY = BKFD_HO_UPDATE_FREQUENCY - elapsed;
 
-  if BKFD_RA_UPDATE_FREQUENCY <= 0 then
-    BKFD_RA_UPDATE_FREQUENCY = 1;
+  if BKFD_HO_UPDATE_FREQUENCY <= 0 then
+    BKFD_HO_UPDATE_FREQUENCY = 1;
 
     local info = TitanHonor.GetCurrencyInfo()
     if (info) then
+      currencyDiscovered = true
       currencyCount = tonumber(info.quantity)
       currencyMaximum = tonumber(info.maxQuantity)
+      seasonalCount = tonumber(info.totalEarned)
+      isSeasonal = info.useTotalEarnedForMaxQty
     end
 
     TitanPanelButton_UpdateButton(TitanHonor.Const.Id)
--- locale/Locale.enUS.lua
+++ locale/Locale.enUS.lua
@@ -1,8 +1,10 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("Honor","enUS",true)
 
-L["BKFD_TITAN_RA_BUTTON_LABEL"] = "Honor: ";
-L["BKFD_TITAN_RA_MENU_TEXT"] = "Honor";
-L["BKFD_TITAN_RA_TOOLTIP_TITLE"] = "Honor";
-L["BKFD_TITAN_RA_TOOLTIP_DESCRIPTION"] = "Used to purchase Unrated PvP\nequipment in Oribos, and to\nupgrade Rated and Unrated PvP\nequipment.";
-L["BKFD_TITAN_RA_TOOLTIP_COUNT_LABEL"] = "Total Maximum: ";
-L["BKFD_TITAN_RA_TOOLTIP_COUNT_LABEL_TOTAL"] = "Total: ";
+L["BKFD_TITAN_HO_BUTTON_LABEL"] = "Honor: ";
+L["BKFD_TITAN_HO_MENU_TEXT"] = "Honor";
+L["BKFD_TITAN_HO_TOOLTIP_TITLE"] = "Honor";
+L["BKFD_TITAN_HO_TOOLTIP_DESCRIPTION"] = "Used to purchase Unrated PvP\nequipment in Oribos, and to\nupgrade Rated and Unrated PvP\nequipment.";
+L["BKFD_TITAN_HO_TOOLTIP_COUNT_LABEL_TOTAL"] = "Total: ";
+L["BKFD_TITAN_HO_TOOLTIP_COUNT_LABEL_TOTAL_MAXIMUM"] = "Total Maximum: ";
+L["BKFD_TITAN_HO_TOOLTIP_COUNT_LABEL_TOTAL_SEASONAL"] = "Season Total Earned: ";
+L["BKFD_TITAN_HO_TOOLTIP_NOT_YET_DISCOVERED"] = "Currency not yet discovered.";
